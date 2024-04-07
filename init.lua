32edeaeea613993e506f86bdfb202041637c26c4
masl <contact@masel.dev>
2024-04-07 02:21:28 +0200

    feat: add selection hotkey for pasted text

--- init.lua
+++ init.lua
@@ -204,6 +204,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Reselect pasted text
+vim.keymap.set('n', 'gp', '`[v`]', {
+  desc = 'Reselect [P]asted text',
+  remap = true,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
